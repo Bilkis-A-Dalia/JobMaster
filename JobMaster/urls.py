81d62aaaae4d12c1c14eedba4288d9c341b47253
BilkisAkterDalia <bilkisadalia@gmail.com>
2024-03-23 23:37:22 +0600

    home and base added

--- JobMaster/urls.py
+++ JobMaster/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from .import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('',views.Home, name='home'),
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- user/urls.py
+++ user/urls.py
@@ -5,8 +5,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('register/',views.register,name = 'register'),
-    path('profile/',views.profile,name='profile'),
-    path('profile/edit',views.edit_profile,name = 'edit_profile'),
+    # path('profile/',views.profile,name='profile'),
+    # path('profile/edit',views.edit_profile,name = 'edit_profile'),
     path('login/',views.user_login,name = 'user_login'),
     path('logout/',views.user_logout,name = 'user_logout'),
     path('active/<uid64>/<token>/', views.activate,name = 'activate'),
